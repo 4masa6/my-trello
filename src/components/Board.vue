86f272a1e8640981821b10a6d7c170284b1c6185
mogata <mogata@kairosmarketing.net>
2021-08-12 21:02:41 +0900

    リストを表示する

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,18 +5,33 @@
         </header>
         <main>
             <p class="info-line">All: 0 tasks</p>
-            <!-- ListAddコンポーネントを呼び出し -->
-            <list-add />
+            <div class="list-index">
+                <list
+                    v-for="(item, index) in lists"
+                    :key="item.id"
+                    :title="item.title"
+                    :listIndex="index"
+                />
+                <list-add />
+            </div>
         </main>
     </div>
 </template>
 
 <script>
 import ListAdd from './ListAdd.vue' // ListAddコンポーネントを読み込み
+import List from './List'
+import { mapState } from 'vuex'
 
 export default {
     components: {
-        ListAdd // コンポーネントに登録する
-    }
+        ListAdd, // コンポーネントに登録する
+        List,
+    },
+    computed: {
+        ...mapState([  // mapStateヘルパー：stateで定義されたデータの名前と同じ名前の文字列で、stateを呼び出すことができる。
+            'lists' // ここではlists
+        ]),
+    },
 }
 </script>
