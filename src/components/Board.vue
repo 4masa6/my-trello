c5b29faef107acdcf42a8f665e531f2868247ee6
mogata <mogata@kairosmarketing.net>
2021-08-12 19:40:05 +0900

    ListAddコンポーネントを作成・Boardコンポーネントに読み込み

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,12 +5,18 @@
         </header>
         <main>
             <p class="info-line">All: 0 tasks</p>
+            <!-- ListAddコンポーネントを呼び出し -->
+            <list-add />
         </main>
     </div>
 </template>
 
 <script>
+import ListAdd from './ListAdd.vue' // ListAddコンポーネントを読み込み
+
 export default {
-    
+    components: {
+        ListAdd // コンポーネントに登録する
+    }
 }
 </script>
