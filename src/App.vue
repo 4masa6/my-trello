7339dc6077e73092c0ad2ee762a002707ab46804
mogata <mogata@kairosmarketing.net>
2021-08-12 19:17:48 +0900

    Boardコンポーネントを作成、App.vueに読み込み

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <!-- Boardコンポーネントを呼び出し -->
+        <board />
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// インポート文追加
+import Board from './components/Board.vue'
 
 export default {
     name: 'App',
     components: {
         HelloWorld
+        // コンポーネントに登録する
+        Board
     }
 }
 </script>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
